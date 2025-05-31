fd315036f4a5e1727851b5f37ef41f46372c05ac
nwninja411 <nwninja0411@gmail.com>
2025-05-31 11:13:09 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+    //Welcome to Javascript Land
+    //Here I am making it so the Javascript handles the header and subheading
+    document.getElementById("Header") .textContent = "Welcome to my simple Webpage";
+    document.getElementById("SubHeading") .textContent = "Get to know me";
+
+    //Lets have the Javascirpt handle my info now.
+    document.getElementById("Name") .textContent = "Name: Noah Walker";
+    document.getElementById("Major") .textContent = "Major: Digital Video";
+    document.getElementById("Grad") .textContent = "Graduation Date: May 2026";
+    document.getElementById("Contact") .textContent = "Contact: Noawalke@uat.edu";
